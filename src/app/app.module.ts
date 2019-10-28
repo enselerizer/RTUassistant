6d13ca7374bc25cbb8a7bf3b2a7bfce5421e1a80
EnselerizeR <enselerizer@gmail.com>
2019-10-28 19:11:30 +0300

    Installing Angular Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { AudioContextModule } from 'angular-audio-context';
     SttFieldComponent
   ],
   imports: [
+    NgbModule,
     BrowserModule,
     AppRoutingModule,
     AudioContextModule.forRoot('balanced')
